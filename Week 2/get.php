a1d48077121e07e000021cfab7b328855da26b1b
liisahy <liisa.hyvonen@metropolia.fi>
2015-03-24 13:35:00 +0200

    Update get.php

--- Week 2/get.php
+++ Week 2/get.php
@@ -20,21 +20,34 @@ try {
     $sql = "SELECT * FROM calendar ORDER BY eDate ASC";
     $STH = $DBH->query($sql);
     $STH->setFetchMode(PDO::FETCH_OBJ);
+		// create standard object which complies with this:
+			//http://fullcalendar.io/docs/event_data/Event_Object/
     while ($row = $STH->fetch()){
-		// create standard object which complies with this: http://fullcalendar.io/docs/event_data/Event_Object/
 		$event = new stdClass();
     	$event->start = $row->eDate;
 		// TODO: you also need
-		// title
-		// description
-		// email
-		// phone
+		// title			
+		//$event->start= $row->title;
+		// description		
+		//$event->start= $row->description;
+		// email			
+		//$event->start= $row->email;
+		// phone			
+		//$event->start= $row->phone;
+		
+		$event->title= $row->eName;
+		$event->description= $row->eDescription;
+		$event->email= $row->pEmail;
+		$event->phone= $row->pPhone;
+		
+		
 		$eventList[] = $event;
     }
 	$eventsJSON = json_encode($eventList);
  } catch (PDOException $e) {
 	echo 'Something went wrong';
-	file_put_contents('log.txt', $e->getMessage()."\n\r", FILE_APPEND); // remember to set the permissions so that log.txt can be created
+	file_put_contents('log.txt', $e->getMessage()."\n\r", FILE_APPEND); 
+	// remember to set the permissions so that log.txt can be created
 }      
 
 ?>
@@ -44,7 +57,37 @@ try {
 var jsonEvents = <?php echo $eventsJSON; ?>;
 console.log(jsonEvents);
 // TODO: use jQuery FullCalendar plugin to display the events in a proper calendar
+
+
 // calendar has to have at least month, week and day views
-// timeformat is 24-hour clock
+$('#calendar').fullCalendar({
+header: {
+		right: 'month, basicWeek, basicDay',
+		center: 'prev, next'
+		
+	},
+	events: jsonEvents,
+	
+	eventClick: function(calEvent,jsEvent, view){
+		alert(' Event: ' + calEvent.title + 
+		'\n Description:' + calEvent.description +
+		'\n Email:' +calEvent.email + 
+		'\n Phone:' +calEvent.phone );
+		
+		
+	},
+	// timeformat is 24-hour clock
+	timeFormat: 'H:mm'
+
+				
+		
+	
+});
+
+
+
+
 // when an event is clicked all the event details (jsonEvents) are displayed	
+
+
 </script>
